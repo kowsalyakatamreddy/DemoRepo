e58ef2755acf8e1e84bb70fef61df949f62fd313
kowsalyakatamreddy <35962287+kowsalyakatamreddy@users.noreply.github.com>
2020-01-29 07:57:25 -0800

    Add files via upload
    commited

---
+++ src/test/java/DemoTest/DemoClass.java
@@ -0,0 +1,21 @@
+package DemoTest;
+
+import org.testng.annotations.Test;
+
+public class DemoClass {
+	
+	
+	@Test
+	
+	public void test() {
+		
+		System.out.println("TestNG");
+		
+		
+		
+		
+	}
+	
+	
+
+}
